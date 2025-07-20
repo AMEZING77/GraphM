e9343860c2e85f018712141c16a28f15e99c780e
AMEZING77 <AMEZING77@outlook.com>
2025-07-21 02:30:55 +0800

    添加项目文件。

---
+++ Src/CStatus.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+
+namespace Src;
+
+public class CStatus : EventArgs
+{
+    public static readonly CStatus OK = new CStatus(ErrorInfoArgs.Empty);
+
+    private ErrorInfoArgs errorInfoArgs = ErrorInfoArgs.Empty;
+
+    public CStatus()
+    {
+
+    }
+
+    public CStatus(ErrorInfoArgs args)
+    {
+        errorInfoArgs = args;
+    }
+
+    public bool IsOk() => string.IsNullOrEmpty(errorInfoArgs?.ErrorCode);
+
+    public bool IsError() => !IsOk();
+
+    private CStatus AddAssign(CStatus cur)
+    {
+        // 如果当前状态已经有错误信息，直接返回当前状态
+        if (!IsOk() || cur.IsOk())
+        {
+            return this;
+        }
+        // 如果当前状态没有错误信息，但传入的状态有错误信息，则更新当前状态
+        errorInfoArgs = cur.errorInfoArgs;
+        return this;
+    }
+
+    public static CStatus operator +(CStatus a, CStatus b)
+    {
+        return a.AddAssign(b);
+    }
+
+
+}
+
+public class ErrorInfoArgs : EventArgs
+{
+    public new static readonly ErrorInfoArgs Empty = new ErrorInfoArgs();
+
+    private ErrorInfoArgs()
+    {
+        ErrorCode = string.Empty;
+    }
+
+    public ErrorInfoArgs(string errorCode, string? languageCode = "zh", string? message = "")
+    {
+        ErrorCode = errorCode ?? throw new ArgumentNullException(nameof(errorCode));
+
+        AddMessage(languageCode, message);
+    }
+
+    public string ErrorCode { get; private set; } = string.Empty;
+
+    private readonly Dictionary<string, string> _messages = Lang.GetAllSupportedLanguages()
+        .ToDictionary(lang => lang, lang => string.Empty);
+
+    /// <summary>
+    /// 根据语言代码添加错误消息。
+    /// </summary>
+    /// <param name="languageCode"></param>
+    /// <param name="message"></param>
+    /// <exception cref="ArgumentException">不支持语言</exception>
+    public void AddMessage(string message, string languageCode = "zh")
+    {
+        if (!Lang.IsSupportedLanguage(languageCode, out var normalized))
+            throw new ArgumentNullException($"UnSupportedLanguage: {languageCode} \t message: {message}");
+
+        _messages.TryAdd(normalized, message);
+    }
+
+
+    /// <summary>
+    /// 获取指定语言的错误消息，如果没有找到则返回默认中文（有的话），或返回外部输入的默认消息。
+    /// </summary>
+    /// <param name="languageCode"></param>
+    /// <param name="defaultMessage"></param>
+    /// <returns></returns>
+    public string GetMessage(string languageCode, string defaultMessage = "Unknown error")
+    {
+        var normalized = Lang.NormalizeLanguageCode(languageCode);
+
+        if (_messages.TryGetValue(normalized, out var message))
+            return message;
+
+        return _messages.Values.FirstOrDefault() ?? defaultMessage;
+    }
+
+
+}
+
+
+
---
+++ Src/Lang.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Src;
+public static class Lang
+{
+    private const string zh_CN = "zh-CN";
+
+    private const string en_US = "en-US";
+
+    /// <summary>
+    /// 内部使用的支持语言列表。
+    /// </summary>
+    private static readonly HashSet<string> SupportedLangs = new HashSet<string>()
+    {
+        zh_CN,
+        en_US
+    };
+
+    /// <summary>
+    /// 将输入的语言代码标准化为支持的格式。
+    /// </summary>
+    /// <param name="languageCode"></param>
+    /// <returns></returns>
+    public static string NormalizeLanguageCode(string languageCode)
+    {
+        if (string.IsNullOrWhiteSpace(languageCode))
+            return "zh-CN";
+
+        // 处理简写形式
+        return languageCode.ToLowerInvariant() switch
+        {
+            "zh" or "zh-cn" => zh_CN,
+            "en" or "en-us" => en_US,
+            _ => zh_CN
+        };
+    }
+
+    /// <summary>
+    /// 返回所有支持的语言代码只读列表。
+    /// </summary>
+    /// <returns></returns>
+    public static IReadOnlyCollection<string> GetAllSupportedLanguages() =>
+        SupportedLangs.ToList().AsReadOnly();
+
+    /// <summary>
+    /// 判断给定的语言代码是否为支持的语言。
+    /// </summary>
+    /// <param name="languageCode"></param>
+    /// <returns></returns>
+    public static bool IsSupportedLanguage(string languageCode, out string normalized)
+    {
+        normalized = NormalizeLanguageCode(languageCode);
+        return SupportedLangs.Contains(normalized);
+    }
+
+
+
+
+
+}
